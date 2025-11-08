5348717d5bd5fe0ba0899d63cba1f5bf77a1b621
Hoover-007 <hoover.best.007@gmail.com>
2025-11-08 22:21:56 +0700

    remove job failed 7

--- app/Models/AdminHasRole.php
+++ app/Models/AdminHasRole.php
@@ -8,18 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class AdminHasRole extends Model
 {
     use HasFactory;
-    
+
     protected $table = 'admin_has_roles';
-    
+
     protected $fillable = [
         'admin_id',
         'role_id'
     ];
-    
+
     public function role(){
         return $this->belongsTo(Role::class ,'role_id');
     }
-    
+
     public function admin(){
         return $this->belongsTo(Admin::class ,'admin_id');
     }
